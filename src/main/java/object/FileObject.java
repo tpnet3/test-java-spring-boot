f74d5f107f829dff53cf987eeba2d7723b107a60
Gyeong-Sik Park <tpnet3@gmail.com>
2017-09-15 19:19:08 +0900

    주석 일부 수정

--- src/main/java/object/FileObject.java
+++ src/main/java/object/FileObject.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 
 public class FileObject {
 
+    // Controller 의 @Valid 로 요청 받을 때 조건을 체크할 수 있습니다.
+    // @NotNull 은 Null 일 경우 오류를 반환하도록 하고, 이와 비슷한 어노테이션이 몇가지 있습니다.
     @NotNull
     private MultipartFile file;
 
